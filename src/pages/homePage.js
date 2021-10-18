b5b120d0cdac8307276bd074d251780940a373ab
Andrew Ivory <andyivory2016@hotmail.com>
2021-10-18 12:04:27 +0100

    Add to favourites logic completed; dummy favourites page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -33,6 +33,12 @@ const handleChange = (type, value) => {
   else setGenreFilter(value);
 };
 
+const addToFavorites = (movieId) => {
+  const updatedMovies = movies.map((m) =>
+    m.id === movieId ? { ...m, favorite: true } : m
+  );
+  setMovies(updatedMovies);
+};
 
 
   useEffect(() => {
@@ -63,7 +69,7 @@ const handleChange = (type, value) => {
       genreFilter={genreFilter}
     />
         </Grid>
-        <MovieList movies={displayedMovies}>
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} >
 </MovieList>
       </Grid>
     </Grid>
