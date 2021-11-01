c25468caf2cb306b3cf38713b7acb9f187042e1e
Andrew Ivory <andyivory2016@hotmail.com>
2021-11-01 12:28:09 +0000

    Added Upcoming movies page.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import SiteHeader from './components/siteHeader'
 import MovieReviewPage from "./pages/movieReviewPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
@@ -15,6 +17,7 @@ const App = () => {
       <Switch>
       <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+        <Route exact path="/movies/upcomingmovies" component={UpcomingMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
         <Route exact path="/" component={HomePage} />
         <Redirect from="*" to="/" />
