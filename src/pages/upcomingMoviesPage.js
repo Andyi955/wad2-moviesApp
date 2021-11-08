9b52c4d5a902fdfd90739c1328146ce74a65bbd7
Andrew Ivory <andyivory2016@hotmail.com>
2021-11-08 21:25:29 +0000

    New button for upcoming ovies page.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import PageTemplate from '../components/templateMovieListPage'
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist'
 
 
   const UpcomingMoviesPage = (props) => {
@@ -19,16 +19,16 @@ import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
     const movies = data.results;
   
     // Redundant, but necessary to avoid app crashing.
-    const favorites = movies.filter(m => m.favorite)
-    localStorage.setItem('favorites', JSON.stringify(favorites))
-    const addToFavorites = (movieId) => true 
+    const playlists = movies.filter(m => m.playlist)
+    localStorage.setItem('playlist', JSON.stringify(playlists))
+    const addToPlaylist = (movieId) => true 
   
     return (
       <PageTemplate
         title="Upcoming Movies"
         movies={movies}
         action={(movie) => {
-          return <AddToFavoritesIcon movie={movie} />
+          return <AddToPlaylistIcon movie={movie} />
         }}
       />    
     );
