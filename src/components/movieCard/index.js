79aede4e0526bcc07a048cee408c570a08b82dc6
Andrew Ivory <andyivory2016@hotmail.com>
2021-11-08 12:23:34 +0000

    Bug Fix - make action icons on movie card configurable

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -26,7 +26,7 @@ const useStyles = makeStyles({
   },
 });
 
-export default function MovieCard({movie}) {
+export default function MovieCard({movie, action}) {
   const classes = useStyles();
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
@@ -84,10 +84,8 @@ export default function MovieCard({movie}) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-      <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
-        <Link to={`/movies/${movie.id}`}>
+      {action(movie)}
+     <Link to={`/movies/${movie.id}`}>
         <Button variant="outlined" size="medium" color="primary">
           More Info ...
         </Button>
