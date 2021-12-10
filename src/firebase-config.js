86813ec5b4df357399dbd8ef55e9276821f7b39d
Andrew Ivory <47563948+Andyi955@users.noreply.github.com>
2021-12-10 21:39:53 +0000

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,13 +5,7 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDTcRgiHPZF5wP8sl-xZHlkZ3pmJ5YkSVc",
-    authDomain: "movie-tmdb-e2c58.firebaseapp.com",
-    projectId: "movie-tmdb-e2c58",
-    storageBucket: "movie-tmdb-e2c58.appspot.com",
-    messagingSenderId: "692633223993",
-    appId: "1:692633223993:web:03218454ceda9b82389ee8",
-    measurementId: "${config.measurementId}"
+  
   };
 
   const app = initializeApp(firebaseConfig);
