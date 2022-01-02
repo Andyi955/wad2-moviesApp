9b6f0335c3fa3e0a03b43db258afb85de7afe955
Andrew Ivory <andyivory2016@hotmail.com>
2022-01-02 17:01:04 +0000

    added routing for movies and movie reviews

--- movies-api/api/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { movies, movieReviews, movieDetails } from './moviesData';
-
-const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(movies);
-});
-
-export default router;
